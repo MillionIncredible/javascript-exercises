57fe2171748c099b28c583e4d8a7c100b33e8504
MillionIncredible <maxi.schwartz94@icloud.com>
2026-03-02 13:48:10 +0100

    solve 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (input) {
+  return input.split("").reverse().join("");
+  //   const inputAsArr = input.split("");
+  //   const reversedArr = [];
 
+  //   for (let i = inputAsArr.length; i >= 0; i--) {
+  //     reversedArr.push(inputAsArr[i]);
+  //   }
+
+  //   return reversedArr.join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// The function should return the input string in reversed order.
