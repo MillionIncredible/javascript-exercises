8634e381edaae4276120290340e58c55de941042
MillionIncredible <maxi.schwartz94@icloud.com>
2026-03-02 14:53:35 +0100

    delete comment

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -6,16 +6,3 @@ const removeFromArray = function (array, ...toRemove) {
 
 // Do not edit below this line
 module.exports = removeFromArray;
-
-// Understand
-// Write a function that takes N parameters from an array and returns
-// the array from which the values have been removed.
-// Plan
-// Iterate through the arguments that should be removed. If one is
-// found in the array, delete it.
-// Pseudocode
-// Loop through remove
-// Check through findIndex if the item is in array.
-// If so, splice it with (index, 1)
-// ---
-//
