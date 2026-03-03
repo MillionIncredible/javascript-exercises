78276f0f0dafc5d99e4a473cb43b75fef36095ac
MillionIncredible <maxi.schwartz94@icloud.com>
2026-03-03 11:34:35 +0100

    solve 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return roundToDecimal((fahrenheit - 32) / 1.8);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return roundToDecimal(celsius * 1.8 + 32);
 };
+
+function roundToDecimal(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
