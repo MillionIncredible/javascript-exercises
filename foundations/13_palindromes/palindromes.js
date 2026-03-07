52eaaa8db3c6e440c0708cb336b2364eec060c79
MillionIncredible <maxi.schwartz94@icloud.com>
2026-03-07 12:45:46 +0100

    solve 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const cleaned = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversedWord = cleaned.split("").reverse().join("");
+  return cleaned === reversedWord;
 };
 
 // Do not edit below this line
